f19b699f8a7164f0b00f754cf4094ffa621c834e
Andrew <andrewsafonovpost@gmail.com>
2025-12-22 21:58:12 +0200

    feat(tasks): add Project and Task models

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Task
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Project(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=200)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    id = models.UUIDField(default=uuid4, unique=True, editable=False, primary_key=True)
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        ordering = ('author', '-created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+
+    class PRIORITY_CHOICES(models.TextChoices):
+        LOW = ('low', 'Low')
+        MEDIUM = ('medium', 'Medium')
+        HIGH = ('high', 'High')
+
+    # Could be added later if functionality for shared projects is implemented
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+
+    title = models.CharField(max_length=200)
+
+    priority = models.CharField(
+        max_length=7, choices=PRIORITY_CHOICES.choices, default=PRIORITY_CHOICES.LOW
+    )
+    due_date = models.DateField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    id = models.UUIDField(default=uuid4, unique=True, editable=False, primary_key=True)
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ('project', '-created')
+
+    def __str__(self):
+        return self.title
