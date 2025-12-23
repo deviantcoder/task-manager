ef179412ff1496ad0ea4cefedce9e88e1fbf8c2e
Andrew <andrewsafonovpost@gmail.com>
2025-12-23 15:14:37 +0200

    feat(tasks): add project list view and template

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Q
 
-# Create your views here.
+
+@login_required
+def project_list(request):
+    projects = (
+        request.user.projects.all()
+        .prefetch_related('tasks')
+        .annotate(
+            active_tasks_count=Count(
+                'tasks', filter=Q(tasks__is_completed=False)
+            ),
+            completed_tasks_count=Count(
+                'tasks', filter=Q(tasks__is_completed=True)
+            )
+        )
+    )
+
+    context = {
+        'projects': projects,
+    }
+
+    return render(request, 'tasks/project_list.html', context)
