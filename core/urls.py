2175fdb8f28e67b93ba05dae23c9a7fd0fb9ad10
Andrew <andrewsafonovpost@gmail.com>
2025-12-25 11:17:16 +0200

    feat(core): redirect root url to project list

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.shortcuts import redirect
+
+
+def root_redirect(request):
+    return redirect('tasks:project_list')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_redirect),
     path('', include('apps.accounts.urls')),
     path('', include('apps.tasks.urls')),
 ]
